69c071d2f99649f062ec5912887dd0f40f5fe793
Mary <80350198+MarySavchuk3@users.noreply.github.com>
2023-01-23 23:43:28 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from importlib.metadata import entry_points
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='0.1.0',
+    description='Clean folder script',
+    url='https://github.com/MarySavchuk3/Module_7.git',
+    author='Savchuk Mariia',
+    author_email='maria.savchuk2013@gmail.com',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts':['clean-folder=clean_folder.main:main_func']}
+   )
